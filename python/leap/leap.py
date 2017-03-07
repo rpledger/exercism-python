56e2edbb124a2c63187e0ddc236cc97719528441
Rebecca Pledger <rebeccapledger14@gmail.com>
2017-03-06 20:52:01 -0600

    Add comments to leap

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,11 +1,20 @@
-
+#
+# Leap function
+# is_leap_year(year)
+# Input: Year
+# Output: True of False
+#
 
 def is_leap_year(year):
+	# Divisible by 4
 	if year % 4 == 0:
+		# Divisible by 100
 		if year % 100 == 0:
+			# Divisible by 400
 			if year % 400 == 0:
 				return True
 			return False
 		return True
+	# Not divisible by 4
 	else:
 		return False
