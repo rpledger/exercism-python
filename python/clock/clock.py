c1caa035135b6b13500fd5a7e796e02de22a70a9
Rebecca Pledger <rebeccapledger14@gmail.com>
2017-03-06 21:32:57 -0600

    Roll over hours more than one

--- python/clock/clock.py
+++ python/clock/clock.py
@@ -2,16 +2,16 @@ class Clock:
 	def __init__(self, hours, minutes):
 		self.hr = hours % 24
 		if minutes >= 60:
-			self.hr = (self.hr + 1) % 24
+			self.hr = (self.hr + (minutes % 60)) % 24
 		self.min = minutes % 60
+		print self.hr
+		print self.min
 
 	def __repr__(self):
 		return "%02d:%02d" % (self.hr, self.min)
 
 	def add(self, min_to_add):
 		if (min_to_add + self.min) >= 60:
-			print self.hr
-			self.hr = (self.hr + 1) % 24
-			print self.hr
+			self.hr = (self.hr + (int((self.min + min_to_add)/60))) % 24
 		self.min = (self.min + min_to_add) % 60
 		return self
