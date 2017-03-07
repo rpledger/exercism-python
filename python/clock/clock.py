7a838f01787a3fe921e9fb2f096c05073952e457
Rebecca Pledger <rebeccapledger14@gmail.com>
2017-03-06 21:19:09 -0600

    Add init and add function

---
+++ python/clock/clock.py
@@ -0,0 +1,16 @@
+class Clock:
+	def __init__(self, hours, minutes):
+		self.hr = hours % 24
+		if minutes >= 60:
+			self.hr = (self.hr + 1) % 24
+		self.min = minutes % 60
+
+	def __str__(self):
+		return "%02d:%02d" % (self.hr, self.min)
+
+	def add(self, min_to_add):
+		if (min_to_add + self.min) >= 60:
+			print self.hr
+			self.hr = (self.hr + 1) % 24
+			print self.hr
+		self.min = (self.min + min_to_add) % 60
