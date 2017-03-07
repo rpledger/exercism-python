e25b5c5fee6debbe7d066e0091dd8f5f5033bf86
Rebecca Pledger <rebeccapledger14@gmail.com>
2017-03-06 21:25:06 -0600

    add __repr__ function and return self from add

--- python/clock/clock.py
+++ python/clock/clock.py
@@ -5,7 +5,7 @@ class Clock:
 			self.hr = (self.hr + 1) % 24
 		self.min = minutes % 60
 
-	def __str__(self):
+	def __repr__(self):
 		return "%02d:%02d" % (self.hr, self.min)
 
 	def add(self, min_to_add):
@@ -14,3 +14,4 @@ class Clock:
 			self.hr = (self.hr + 1) % 24
 			print self.hr
 		self.min = (self.min + min_to_add) % 60
+		return self
