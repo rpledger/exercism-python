f2d2f4713f05333c75dd97b023d4260458b5fd8e
Rebecca Pledger <rebeccapledger14@gmail.com>
2017-03-09 19:14:07 -0600

    string(ascii_letters) instead of typing out alphabet

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,7 +1,6 @@
 # pangram
-
-alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
+import string
+alph = list(string.ascii_letters)
 
 def is_pangram(sentence):
 	if not sentence:
