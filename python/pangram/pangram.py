5b29d675f022087c97a53387f6a0c790c7097abf
Rebecca Pledger <rebeccapledger14@gmail.com>
2017-03-09 19:09:34 -0600

    All 9 tests passing

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -4,20 +4,18 @@ alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o
 
 
 def is_pangram(sentence):
+	if not sentence:
+		return False
 	sent = sentence.lower()
-	sent = list(sentence)
+	sent = list(set(sent))
 	sent.sort()
 	i = 0
 	for s in sent:
-		if s.isalpha() is False:
-			sent.remove(s)
+		if (s.isalpha() is False) or (s == ' '):
 			next
 		else:
 			if s == alph[i]:
 				i += 1
 			else:
 				return False
-	if not sent:
-		return False
-	else:
-		return True
+	return True
